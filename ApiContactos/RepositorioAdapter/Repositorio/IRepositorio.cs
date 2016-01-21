932bc856380cf0d5438ea238f0744b0f9d0216eb
Luis Felipe Gil Lamaignere <luis.gil@toolents.com>
2016-01-21 09:35:41 +0100

    Por fin aggggg

---
+++ ApiContactos/RepositorioAdapter/Repositorio/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace RepositorioAdapter.Repositorio
+{
+    public interface IRepositorio<TEntity,TModel,TAdapter>
+    {
+        TModel Add(TModel model);
+        int Delete(params object[] keys);
+        int Delete(TModel model);
+        int Delete(Expression<Func<TModel, bool>>  consulta);
+        int Update(TModel model);
+        TModel Get(params object[] keys );
+        ICollection<TModel> Get(Expression<Func<TModel, bool>> consulta);
+        ICollection<TModel> Get();
+    }
+}
